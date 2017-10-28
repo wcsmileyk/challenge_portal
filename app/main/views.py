a6c62a2b99b26aa48d4915b2e2d7789046782160
Wes Smiley <wesley.c.smiley@gmail.com>
2017-10-28 13:53:58 -0400

    Minor updates to index

--- app/main/views.py
+++ app/main/views.py
@@ -13,6 +13,12 @@ def index():
     return render_template('index.html', current_challenge=current_challenge, past_challenges=past_challenges)
 
 
+@main.route('/<challenge>')
+def challenge_detailed(challenge):
+    # current_challenge = Challenge.query.filter_by(id=challenge).first()
+    return render_template('challenge.html')
+
+
 @main.route('/<user>')
 def profile(user):
     pass
