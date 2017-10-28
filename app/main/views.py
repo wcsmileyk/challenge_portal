b1d3aa657353fadb00dd64bbfb5f81695e4c8106
Wes Smiley <wesley.c.smiley@gmail.com>
2017-10-28 14:35:08 -0400

    Added pagination to index

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request, current_app
 
 from . import main
 from ..models import Challenge
@@ -6,11 +6,12 @@ from ..models import Challenge
 
 @main.route('/')
 def index():
-
     current_challenge = Challenge.query.order_by(Challenge.timestamp.desc()).first()
-    past_challenges = Challenge.query.order_by(Challenge.timestamp.desc()).offset(1).all()
+    page = request.args.get('page', 1, type=int)
+    pagination = Challenge.query.order_by(Challenge.timestamp.desc()).paginate(page, per_page=5, error_out=False)
+    past_challenges = pagination.items
 
-    return render_template('index.html', current_challenge=current_challenge, past_challenges=past_challenges)
+    return render_template('index.html', current_challenge=current_challenge, past_challenges=past_challenges, pagination=pagination)
 
 
 @main.route('/<challenge>')
